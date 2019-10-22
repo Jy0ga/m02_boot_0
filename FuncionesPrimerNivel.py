1591e90baea890294e11748d4b00a1ed14268ec0
David Rivero Lago <riverolago@outlook.es>
2019-10-22 19:03:41 +0200

    Lambdas

--- FuncionesPrimerNivel.py
+++ FuncionesPrimerNivel.py
@@ -3,7 +3,10 @@ def normal(x):
 
 def cuadrado (x):
     return x*x
-    
+
+def cubo(x):
+    return x**3    # "**" = operador de potencia
+
 def sumaTodos(limitTo, f):
     resultado = 0
     for i in range(limitTo+1):
@@ -11,6 +14,6 @@ def sumaTodos(limitTo, f):
     
     return resultado
 
-
-print(sumaTodos(100, normal))
-print(sumaTodos(3, cuadrado))
+if __name__ == "__main__":
+    print(sumaTodos(100, normal))
+    print(sumaTodos(3, cuadrado))
---
+++ funcionesLambda.py
@@ -0,0 +1,7 @@
+from FuncionesPrimerNivel import sumaTodos 
+
+doble = lambda x:x*2
+triple = lambda x: x*3
+
+print(sumaTodos(3, doble))
+print(sumaTodos(3, triple))
